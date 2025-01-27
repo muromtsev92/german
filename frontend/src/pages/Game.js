4d94f4b03dcb82ae67349b6b5ee3bc17a8a88f84
svmuromtsev <k29393@yandex.ru>
2025-01-28 00:02:31 +0500

    add front

---
+++ frontend/src/pages/Game.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Game = () => {
+  return (
+    <div>
+      <h1>Игра</h1>
+      <p>Тут вы сможете практиковаться в заучивании немецких слов.</p>
+    </div>
+  );
+};
+
+export default Game;
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Добро пожаловать!</h1>
+      <p>Это главная страница приложения.</p>
+    </div>
+  );
+};
+
+export default Home;
