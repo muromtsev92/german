bb85ea68f3de1baf0c0b13deca53eb6cdb07749c
svmuromtsev <k29393@yandex.ru>
2025-02-01 13:40:58 +0500

    article game works correct

--- backend/src/main/java/com/example/backend/config/WebConfig.java
+++ backend/src/main/java/com/example/backend/config/WebConfig.java
@@ -13,12 +13,15 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+                        .allowedOrigins("http://localhost:3000", "http://37.46.16.152", "http://german-frontend")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
 }
+
 
 //"http://localhost:3000"
 //"http://37.46.16.152"
